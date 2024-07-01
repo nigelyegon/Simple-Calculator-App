711957709ce2371c5381a2de3650ef969a5e70b9
nigelyegon <nigelkimutai@gmail.com>
2024-07-01 17:38:35 +0300

    Shorthand tests

--- test/basic_math_test.exs
+++ test/basic_math_test.exs
@@ -2,16 +2,12 @@ defmodule BasicMathTest do
   use ExUnit.Case
   doctest BasicMath
 
-  test "add 2 + 3 = 5" do
-    assert BasicMath.add(2, 3) == 5
-  end
-  test "subtract 5 - 2 = 3" do
-    assert BasicMath.subtract(5, 2) == 3
-  end
-  test "multiply 2 * 3 = 6" do
-    assert BasicMath.multiply(2, 3) == 6
-  end
-  test "divide 6 / 2 = 3" do
-    assert BasicMath.divide(6, 2) == 3
-  end
+  test "add 2 + 3 = 5", do: assert BasicMath.add(2, 3) == 5
+
+  test "subtract 5 - 2 = 3", do: assert BasicMath.subtract(5, 2) == 3
+
+  test "multiply 2 * 3 = 6", do: assert BasicMath.multiply(2, 3) == 6
+
+  test "divide 6 / 2 = 3", do: assert BasicMath.divide(6, 2) == 3
+
 end
